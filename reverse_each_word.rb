5d8c7f0ddda99481964b0b97172285292db66516
Takeshi Kimoto <lnuk2009jp@gmail.com>
2019-08-20 10:34:50 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,5 +2,5 @@ def reverse_each_word(string)
  array = string.split(" ")
  new_array = []
  array.each {|array_element| new_array.push(array_element.reverse)}
- new_array.join()
+ new_array.join(" ")
 end
